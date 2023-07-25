7083b92f0999e82b28d7e57c52636c784cc897c9
bfalad22 <bolasaliu93@gmail.com>
2023-07-24 20:00:39 -0700

    API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Import request module """
+import requests
+from sys import argv
+
+
+def userTodo():
+    """ returns information about his/her TODO list progress. """
+    api = "https://jsonplaceholder.typicode.com/"
+    if (len(argv) > 1):
+        id = int(argv[1])
+        reqUser = requests.get('{}users/{:d}'.format(api, id)).json()
+        reqTodo = requests.get('{}todos'.format(api),
+                               params={'userId': id}).json()
+        todoComplete = list(filter(lambda x: x.get('completed'), reqTodo))
+
+        name = reqUser.get('name')
+        print('Employee {} is done with tasks({:d}/{:d}):'
+              .format(name, len(todoComplete), len(reqTodo)))
+        for i in todoComplete:
+            print('\t {}'.format(i.get('title')))
+
+
+if __name__ == "__main__":
+    userTodo()
